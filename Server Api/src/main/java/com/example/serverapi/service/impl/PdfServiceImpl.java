5931e781e3c78e9135ac6da7ab974ce903a5bab1
Nobleman2704 <turayevasad@gmail.com>
2024-03-02 10:47:28 +0500

    pdf api fixed 1

--- Server Api/src/main/java/com/example/serverapi/service/impl/PdfServiceImpl.java
+++ Server Api/src/main/java/com/example/serverapi/service/impl/PdfServiceImpl.java
@@ -18,8 +18,8 @@ import static com.example.serverapi.util.PdfUtil.getCell;
 public class PdfServiceImpl implements PdfService {
     @Override
     public byte[] downloadProducts(List<ResProductDto> dtoList) {
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream();) {
-            Document document = new Document(PageSize.A4, 7f, 7f, 7f, 7f);
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            Document document = new Document(PageSize.A4.rotate(), 7f, 7f, 7f, 7f);
 
             PdfWriter writer = PdfWriter.getInstance(document, out);
 
@@ -52,40 +52,40 @@ public class PdfServiceImpl implements PdfService {
 
         PdfPTable pdfPTable = new PdfPTable(9);
         pdfPTable.setWidthPercentage(100f);
-        pdfPTable.setWidths(new float[]{1f, 1.3f, 3f, 1.5f, 2f, 2.2f, 3f, 2.2f, 3f});
+        pdfPTable.setWidths(new float[]{0.8f, 1.1f, 3f, 1.2f, 1.3f, 2.5f, 3f, 2.5f, 3f});
 
 
         PdfPCell headerCell = new PdfPCell(new Paragraph("Products", fontHeader18));
         headerCell.setColspan(9);
-        headerCell.setPaddingBottom(5f);
+        headerCell.setPaddingBottom(10f);
         headerCell.setBorder(Rectangle.NO_BORDER);
         headerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
         headerCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 
         pdfPTable.addCell(headerCell);
 
-        pdfPTable.addCell(getCell("t/r", font14, false));
-        pdfPTable.addCell(getCell("Id", font14, false));
-        pdfPTable.addCell(getCell("Name", font14, true));
-        pdfPTable.addCell(getCell("Price", font14, false));
-        pdfPTable.addCell(getCell("Amount", font14, false));
-        pdfPTable.addCell(getCell("Created", font14, false));
-        pdfPTable.addCell(getCell("Created By", font14, true));
-        pdfPTable.addCell(getCell("Updated", font14, false));
-        pdfPTable.addCell(getCell("Updated By", font14, true));
+        pdfPTable.addCell(getCell("№", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Id", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Name", font14, BaseColor.LIGHT_GRAY, 0.9f, true));
+        pdfPTable.addCell(getCell("Price", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Amount", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Created", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Created By", font14, BaseColor.LIGHT_GRAY, 0.9f, true));
+        pdfPTable.addCell(getCell("Updated", font14, BaseColor.LIGHT_GRAY, 0.9f, false));
+        pdfPTable.addCell(getCell("Updated By", font14, BaseColor.LIGHT_GRAY, 0.9f, true));
 
         for (int i = 0; i < dtoList.size(); i++) {
             ResProductDto resProductDto = dtoList.get(i);
 
-            pdfPTable.addCell(getCell(String.valueOf(i + 1), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getId().toString(), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getName(), font12, true));
-            pdfPTable.addCell(getCell(resProductDto.getPrice().toString(), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getAmount().toString(), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getCreated().toString(), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getCreatedBy(), font12, true));
-            pdfPTable.addCell(getCell(resProductDto.getUpdated().toString(), font12, false));
-            pdfPTable.addCell(getCell(resProductDto.getUpdatedBy(), font12, true));
+            pdfPTable.addCell(getCell(String.valueOf(i + 1), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getId().toString(), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getName(), font12, BaseColor.WHITE, 0.5f, true));
+            pdfPTable.addCell(getCell(resProductDto.getPrice().toString(), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getAmount().toString(), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getCreated().toString(), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getCreatedBy(), font12, BaseColor.WHITE, 0.5f, true));
+            pdfPTable.addCell(getCell(resProductDto.getUpdated().toString(), font12, BaseColor.WHITE, 0.5f, false));
+            pdfPTable.addCell(getCell(resProductDto.getUpdatedBy(), font12, BaseColor.WHITE, 0.5f, true));
         }
 
         return pdfPTable;
--- Server Api/src/main/java/com/example/serverapi/util/PdfUtil.java
+++ Server Api/src/main/java/com/example/serverapi/util/PdfUtil.java
@@ -1,13 +1,16 @@
 package com.example.serverapi.util;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 
 public class PdfUtil {
-    public static PdfPCell getCell(String text, Font font, boolean isWordCell) {
+    public static PdfPCell getCell(String text, Font font, BaseColor baseColor, float borderWith, boolean isWordCell) {
         PdfPCell cell = new PdfPCell(new Paragraph(text, font));
+        cell.setBackgroundColor(baseColor);
+        cell.setBorderWidth(borderWith);
         cell.setHorizontalAlignment(isWordCell ? Element.ALIGN_LEFT : Element.ALIGN_CENTER);
         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
         return cell;
